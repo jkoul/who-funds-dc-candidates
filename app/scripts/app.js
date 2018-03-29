0b0387ff662bcf99d868933c7961673632c47508
Jeremy Koulish <jkoulish@gmail.com>
2018-03-29 17:38:59 -0400

    initial yo angular build

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name whofundsdcApp
+ * @description
+ * # whofundsdcApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('whofundsdcApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngSanitize'
+  ]);
