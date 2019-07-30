141e64491186a3e63eb7b99abe041f1fa7f394d9
Jeremy Koulish <jkoulish@gmail.com>
2019-07-29 21:38:03 -0400

    actually delete obsolete data & service files and their references within the app

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,8 +11,7 @@ angular.module('app')
   .controller('MainCtrl', function ($scope, $filter, mainData) {
     $scope.main = {
       races: mainData[0].data,
-      candidates: mainData[1].data,
-      campaigns: mainData[2].data,
+      campaigns: mainData[1].data,
       electionYears: $filter('electionYears')(mainData[0].data)
     };
   });
